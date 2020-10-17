ede99240e5916d6d4e72ef0279b8dc23ef6b7bbb
stevejhiggs <stevejhiggs@hotmail.com>
2020-10-17 18:19:49 +0100

    Add styled component support

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,4 +1,14 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  border-radius: 3px;
+  border: 2px solid DodgerBlue;
+  color: white;
+  background-color: CornflowerBlue;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
 
 const Counter: React.FC = () => {
   const [count, setCount] = useState(0);
@@ -6,8 +16,8 @@ const Counter: React.FC = () => {
     <>
       <h2>You clicked {count} times!</h2>
 
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <Button onClick={() => setCount(count - 1)}>Decrement</Button>
+      <Button onClick={() => setCount(count + 1)}>Increment</Button>
     </>
   );
 };
